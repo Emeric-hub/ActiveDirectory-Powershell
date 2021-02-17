2bcb888410d4a56627e22e13eb968d3ed5765435
Emeric-hub <79230405+Emeric-hub@users.noreply.github.com>
2021-02-17 23:48:21 +0100

    Add files via upload

---
+++ O365/00_Export_ProxyAdress_from_Azure.ps1
@@ -0,0 +1,13 @@
+﻿# EDE - le 14/09/20
+#Script avec pour objectif de recuperer les smtp proxy des utilisateurs coté Azure AD
+
+#CmdLet a installer
+#Install-Module MSOnline
+
+#Connection Azure
+Connect-MsolService -Credential $credential
+
+get-msoluser | select DisplayName , UserPrincipalName, @{L = "ProxyAddresses"; E = { $_.ProxyAddresses -join ";"}} | export-csv -append .\Export_proxy.csv -delimiter ";"
+
+#Get-MSOLUser | Select DisplayName , UserPrincipalName, @{Name="PrimaryEmailAddress";Expression={$_.ProxyAddresses | ?{$_ -cmatch '^SMTP\:.*'}}} | export-csv -append "C:\Users\edelavaud\Desktop\O365 - Ransonware\Messagerie\ExportTest.csv"
+
